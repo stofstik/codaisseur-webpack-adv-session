89377a29572260059b6d8048f4cda7f043b51838
stofstik <verstegen.daan@gmail.com>
2016-12-15 13:46:14 +0100

    fixed code styling

--- src/Balloon.js
+++ src/Balloon.js
@@ -5,16 +5,16 @@ import './Balloon.sass'
 
 export default class Balloon {
   static colors = [
-    "#1f77b4",
-    "#ff7f0e",
-    "#2ca02c",
-    "#d62728",
-    "#9467bd",
-    "#8c564b",
-    "#e377c2",
-    "#7f7f7f",
-    "#bcbd22",
-    "#17becf"
+    '#1f77b4',
+    '#ff7f0e',
+    '#2ca02c',
+    '#d62728',
+    '#9467bd',
+    '#8c564b',
+    '#e377c2',
+    '#7f7f7f',
+    '#bcbd22',
+    '#17becf'
   ]
 
   constructor(props = {}) {
@@ -43,7 +43,7 @@ export default class Balloon {
 
     container.animate({
       bottom: '100%',
-      left: xdiff + "%"
+      left: xdiff + '%'
     }, (8000 - width * 10))
   }
 
